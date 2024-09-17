59edc77f4167d45abc7e34cdc4b45feb27f61111
salah salah <s.salahs1e@gmail.com>
2024-09-17 13:39:50 -0400

    changes to logo and background

---
+++ src/components/Background.jsx
@@ -0,0 +1,13 @@
+import React from 'react' ;
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+
+const Background = () => {
+  return (
+    <div>text</div>
+    )
+}
+
+export default Background
