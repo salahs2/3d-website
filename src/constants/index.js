921de4d9ef93b91d0ac7b4bbae22b2d84e5647f1
salah salah <s.salahs1e@gmail.com>
2025-03-18 21:04:03 -0400

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -118,15 +118,27 @@ import {
   
   const experiences = [
     {
-      title: "Research Assistant",
+      title: "Statistical Research Assistant",
+      company_name: "McMaster University Hospital - Health Research, Evidence, and Impact",
+      icon: mcmaster,
+      iconBg: "#383E56",
+      date: "Jun 2023 - Dec 2023",
+      points: [
+        "Analyzed health data with over 100,000 columns to assess occupational health risks in specific demographic groups",
+        "Deployed a match-back algorithm in R using ISCO2008 job code classification, automating the manual data cleaning and categorization process, and achieving a 70% accuracy in matching job roles to standardized codes",
+        "Implemented a Levenshtein distance algorithm using Python & PostgreSQL to optimize data duplication, reducing unique columns by 50%, from over 16,000 to 8,000, significantly streamlining the dataset for analysis",
+      ],
+    },
+    {
+      title: "Software Engineering Co-op",
       company_name: "McMaster University",
       icon: mcmaster,
       iconBg: "#383E56",
       date: "May 2023 - Aug 2023",
       points: [
-        "Conducted statistical analysis using R on student evaluation of physics courses analyzing grades, pass rates, establishing difficulty classification, and creating scenario analysis on new grading/marking schemes and expected results",
-        "Created Levenshtein distance algorithm using python to reduce unique values of a dataset by ~50% from >16,000 unique values to ~8,000",
-        "Developed Test Bank Database for Physics courses to organize previous tests and questions used in examination for data collection",
+        "Created Front End UI of Test Database software using TypeScript to create React Components",
+        "Deployed early version of the product as a desktop application using Electron.js managing user accessibility interactions & authentication using Google Cloud’s Firebase",
+        "Conducted statistical analyses in R on student evaluations, assessing course difficulty. Scenario analysis of grading  schemes led to a 3–4% average grade improvement, from 69% to 73% in the following term",
       ],
     },
     {
@@ -138,20 +150,20 @@ import {
       points: [
         "Evaluating minority investment in financial services company seeking to raise $100mm in preferred equity by assessing Y3 MOIC and IRR (withdrawn due to ESG Concerns) ",
         "Conducting diligence on sale of controlled portfolio company valued at ~$4B by evaluating strategic buyers and calculating potential sale multiple using comparable analysis & analyzing potential synergies with strategic buyers resulting in initiation of sale proces",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Utilized Bloomberg API with Python to retrieve and analyze private equity fund performance data, conducting advanced screens to benchmark portfolio returns against average market performance",
       ],
     },
     {
-      title: "Risk Management Intern",
+      title: "Quantitative Risk Engineer Intern",
       company_name: "Fidelity",
       icon: fidelity,
       iconBg: "#E6DEDD",
       date: "Jan 2022 - Jan 2023",
       points: [
-        "Created a database for enterprise risk assessments monitoring risk classifications, and updating re-assessments resulting in efficient risk assessment and increased accuracy in enterprise risk monitoring",
+        "Engineered system using Microsoft Power Automate SaaS Platform, Python, and HTTPX to scrape news articles for risk-related keywords, reducing monitoring time and enhancing risk detection for global events.",
         "Created Microsoft Power Automate system that screens relevant news articles that pose measured risk levels based on keywords generating a confidence interval of negative articles to screen for unpredicted developments relevant to vendor risk, reducing news monitoring time by >90%",
         "Assisted with Thesis publication on sector-specific technical indicators to assist Portfolio Managers decision-making process",
-        "Developed dashboard for Portfolio Managers to identify quantitative indicators using Python Dash, Plotly and Pandas frameworks allowing Portfolio Managers to back test specific parameters without needing programming expertise",
+        "Built a Python-based dashboard using Dash, NumPy, Matplotlib, Plotly, and Pandas displaying statistical information on proprietary models including indexing, normalization, optimization & significance results",
       ],
     },
     
